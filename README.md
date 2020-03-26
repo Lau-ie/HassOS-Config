ca87b3a7f5a8afe7b467d8d68f4dea509adda1f7
git <root@a0d7b954-vscode.local.hass.io>
2020-03-26 18:20:57 +0100

    the first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Lau-ie's Home Assistant Configuration
+
+## About
+
+This is my personal Home Assistant configuration.
+
+Follow me on my journey and be sure to hit the GitHub :star2:
+
+## Screenshot's
+
+Just some screenshots of my lovelace frontend:
+
+![Main overview](https://i.imgur.com/lPh3RFD.png)
+
+![Lights & Sockets](https://i.imgur.com/Exfc0ja.pngg)
+
+![Sensors](https://i.imgur.com/VR7Wk1h.png)
+
+![Adguard & HASS-control](https://i.imgur.com/I230EZI.png)
+
+## License
+
+MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+[commits-shield]: https://img.shields.io/github/commit-activity/y/Lau-ie/HassOS-config.svg
+[commits]: https://github.com/Lau-ie/HassOS-config/commits/master
+[gitlabci-shield]: https://gitlab.com/Lau-ie/HassOS-config/badges/master/pipeline.svg
+[gitlabci]: https://gitlab.com/Lau-ie/HassOS-config/pipelines
+[home-assistant]: https://home-assistant.io
+[issue]: https://github.com/Lau-ie/HassOS-config/issues
+[maintenance-shield]: https://img.shields.io/maintenance/yes/2019.svg
+[last-commit-shield]: https://img.shields.io/github/last-commit/Lau-ie/HassOS-config.svg
+[stars-shield]: https://img.shields.io/github/stars/Lau-ie/HassOS-config.svg?style=social&label=Stars
+[watchers-shield]: https://img.shields.io/github/watchers/Lau-ie/HassOS-config.svg?style=social&label=Watchers
---
+++ www/singlevalue-card.js
@@ -0,0 +1,99 @@
+class SingleValueCard extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+  setConfig(config) {
+    if (!config.entity) {
+      throw new Error('Please define an entity');
+    }
+
+    const root = this.shadowRoot;
+    if (root.lastChild) root.removeChild(root.lastChild);
+    const cardConfig = Object.assign({}, config);
+    if (!cardConfig.scale) cardConfig.scale = "50px";
+    if (!cardConfig.from) cardConfig.from = "left";
+    const card = document.createElement('ha-card');
+    const content = document.createElement('div');
+    content.id = "value"
+    const style = document.createElement('style');
+    style.textContent = `
+      ha-card {
+        box-shadow: none;
+        text-align: left;
+        --singlevalue-fill-color: var(--label-badge-blue);
+        --singlevalue-percent: 100%;
+        --singlevalue-direction: ${cardConfig.from};
+        --base-unit: ${cardConfig.scale};
+        padding: calc(var(--base-unit)*0.8) calc(var(--base-unit)*0.8);
+        background: linear-gradient(to var(--singlevalue-direction), var(--paper-card-background-color) var(--singlevalue-percent), var(--singlevalue-fill-color) var(--singlevalue-percent));
+      }
+      #value {
+        font-size: calc(var(--base-unit) * 1.0);
+        line-height: calc(var(--base-unit) * 1.0);
+        color: var(--primary-text-color);
+      }
+    `;
+    card.appendChild(content);
+    card.appendChild(style);
+    card.addEventListener('click', event => {
+      this._fire('hass-more-info', { entityId: cardConfig.entity });
+    });
+    root.appendChild(card);
+    this._config = cardConfig;
+  }
+
+  _fire(type, detail, options) {
+    const node = this.shadowRoot;
+    options = options || {};
+    detail = (detail === null || detail === undefined) ? {} : detail;
+    const event = new Event(type, {
+      bubbles: options.bubbles === undefined ? true : options.bubbles,
+      cancelable: Boolean(options.cancelable),
+      composed: options.composed === undefined ? true : options.composed
+    });
+    event.detail = detail;
+    node.dispatchEvent(event);
+    return event;
+  }
+
+  _computeSeverity(stateValue, sections) {
+    const numberValue = Number(stateValue);
+    let style;
+    sections.forEach(section => {
+      if (numberValue <= section.value && !style) {
+        style = section.style;
+      }
+    });
+    return style || 'var(--label-badge-blue)';
+  }
+
+  _translatePercent(value, min, max) {
+    return 100-100 * (value - min) / (max - min);
+  }
+
+  set hass(hass) {
+    const config = this._config;
+    const root = this.shadowRoot;
+    const entityState = hass.states[config.entity].state;
+    const measurement = hass.states[config.entity].attributes.unit_of_measurement || "";
+
+    if (entityState !== this._entityState) {
+      if (config.min !== undefined && config.max !== undefined) {
+        root.querySelector("ha-card").style.setProperty('--singlevalue-percent', `${this._translatePercent(entityState, config.min, config.max)}%`);
+      }
+      if (config.severity) {
+        root.querySelector("ha-card").style.setProperty('--singlevalue-fill-color', `${this._computeSeverity(entityState, config.severity)}`);
+      }
+      root.getElementById("value").textContent = `${entityState} ${measurement}`;
+      this._entityState = entityState;
+    }
+    root.lastChild.hass = hass;
+  }
+
+  getCardSize() {
+    return 1;
+  }
+}
+
+customElements.define('singlevalue-card', SingleValueCard);
